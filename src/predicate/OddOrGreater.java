8ad52c01cdca9516f48e83d4aa25ee0c61227a3a
Rajesh Kumar Behera <rajeshkumarbeheracool@gmail.com>
2025-09-03 13:13:08 +0530

    Display the number from an array which is Odd and greater than 10

---
+++ src/predicate/OddOrGreater.java
@@ -0,0 +1,21 @@
+
+//Display the number from an array which is Odd and greater than 10
+
+package predicate;
+
+import java.util.function.Predicate;
+
+public class OddOrGreater {
+
+	public static void main(String[] args) {
+		int[] arr= {2,5,19,10,13,4,6};
+		Predicate<Integer> pr1 = n -> n % 2 != 0;
+		Predicate<Integer> pr2 = n -> n > 10;
+		for(int num : arr) {
+			if(pr1.and(pr2).test(num)) {
+				System.out.println(num +" ");
+			}
+		}
+	}
+
+}
