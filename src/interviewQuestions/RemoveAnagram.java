35b70da7009a4fbd956087eaa5be239925b58f75
Rajesh Kumar Behera <rajeshkumarbeheracool@gmail.com>
2025-12-11 23:09:39 +0530

    Remove Anagram Update

--- src/interviewQuestions/RemoveAnagram.java
+++ src/interviewQuestions/RemoveAnagram.java
@@ -1,6 +1,63 @@
 package interviewQuestions;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RemoveAnagram {
+
+    public static void main(String[] args) {
+    	String[] words= {"cat","nitin","Raj","NItin","tAc","Jaga","man","NaM"};
+    	Map<String, List<String>> groupedWords= 
+    			Arrays.stream(words)
+    				.collect(Collectors.groupingBy(word -> {
+    					char[] ch= word.toLowerCase().toCharArray();
+    					Arrays.sort(ch);
+    					return new String(ch);
+    				}
+    			));
+    	
+    	System.out.println(groupedWords);
+    	
+    	List<String> result = groupedWords.values().stream()
+    		.filter(value -> value.size() == 1)
+    		.flatMap(List::stream)
+    		.collect(Collectors.toList());
+    	System.out.println(result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -34,3 +91,6 @@ public class RemoveAnagram {
 	}
 
 }
+*/
+
+
